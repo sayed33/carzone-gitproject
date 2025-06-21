440815a76b990d294ddf2909b8982686cf02c07a
sayed33 <elsayedhamdifouda@gmail.com>
2025-06-21 15:47:54 +0400

    completed team and admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Team
+from django.utils.html import format_html
+
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius:10px;" />'.format(object.photo.url))
+
+    thumbnail.short_description ='photo'
+    list_display =('id','thumbnail','first_name','designation','phone','email','created_date')
+    list_display_links =('id','thumbnail','first_name',)
+    search_fields =('first_name','designation','last_name')
+    list_filter =('designation',)
+
+admin.site.register(Team,TeamAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=100)
+    designation = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%M/%D/')
+    facebook = models.URLField(max_length=255)
+    linkedin = models.URLField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
